43149357adc3196712007adaefb6db143fd4240e
askoenig <askoenig94@gmail.com>
2019-05-28 19:55:11 -0400

    Create User/new and User/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to user_path
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :age, :username, :password, :email)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  attr_reader :password
+
+  def current_user
+    session[:user_id] ||= nil
+  end
+
+  def password=(password)
+    @password = password
+    salt = BCrypt::Engine::generate_salt
+    hashed = BCrypt::Engine::hash_secret(password, salt)
+    self.password_digest = salt + hashed
+  end
+
+  def authenticate(password)
+    salt = password_digest[0..28]
+    hashed = BCrypt::Engine::hash_secret(password, salt)
+    return nil unless (salt + hashed) == self.password_digest
+  end
+
 end
