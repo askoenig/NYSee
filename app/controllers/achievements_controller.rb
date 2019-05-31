001028144a2eb913248a6f68d422e25c34ad6f3c
Wei <theweifeng@gmail.com>
2019-05-31 12:00:17 -0400

    Correct method names

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,3 +1,3 @@
 class AchievementsController < ApplicationController
-
+  
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,7 +2,7 @@ class Destination < ApplicationRecord
   has_many :visits
   has_many :users, through: :visits
 
-  def self.most_popular_destinations
+  def self.most_popular_destination
     hash = Hash.new{0}
 
     Visit.all.each {|visit| hash[visit.destination_id] += 1}
@@ -12,6 +12,6 @@ class Destination < ApplicationRecord
     Destination.find(most_popular).name
   end   
 
-  
+
 
 end
