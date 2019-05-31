af7fbff25f90291c45aa62d970e2e58ac994ff87
askoenig <askoenig94@gmail.com>
2019-05-31 12:02:16 -0400

    Add stats to destinations

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,16 +2,16 @@ class Destination < ApplicationRecord
   has_many :visits
   has_many :users, through: :visits
 
-  def self.most_popular_destinations
+  def self.most_popular_destination
     hash = Hash.new{0}
 
     Visit.all.each {|visit| hash[visit.destination_id] += 1}
 
-    most_popular = hash.max_by{|key, val| val}
-    
-    Destination.find(most_popular).name
-  end   
+    most_popular = hash.max_by{|key, val| val}[0]
 
-  
+    Destination.find(most_popular).name
+  end
+
+
 
 end
